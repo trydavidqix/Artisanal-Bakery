d1403144d44b68a4be256974b4c0571ea9111ed5
David William <davidwilliammacedo@icloud.com>
2025-05-15 13:08:05 +0100

    Atualização da URL base para ArtisanalBakery

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/Artisanal-Bakery/',
+  base: '/ArtisanalBakery/',
 
   plugins: [
     react({
